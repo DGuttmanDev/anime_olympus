be212d23190bc3a390af3e624b7948ef6e9ac9aa
dguttman <dguttmandevelop@gmail.com>
2023-04-11 16:28:25 +0200

    job se ejecuta todos los dias a las 12pm, primero comprueba que no existan ningun otro job que esta almacenado en arreglo y en el caso de ser afirmativo los cancela y vacia el arreglo.Luego obtiene todos los animes del archivo .json y guarda en un arreglo los animes que se emitiran en el respectivo dia. Recorre el arreglo de los animes del dia y genera jobs por cada anime. Cada job emite un embed a un determinado chat en cuando cumple el temporizador.

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 const {Client, IntentsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder} = require('discord.js');
-const client = new Client({intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers]});
 const config = require('./config.json');
 const schedule = require('node-schedule');
 const animesJSON = require('./animes.json');
+
+const client = new Client({intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers]});
 
 interface Anime {
     nombre: string;
@@ -18,45 +19,97 @@ interface Anime {
     };
 }
 
-const animes: Anime[] = animesJSON.map((animeJSON: any) => ({
-    nombre: animeJSON.nombre,
-    descripcion: animeJSON.descripcion,
-    thumbnail: animeJSON.thumbnail,
-    url: {
-        animeflv: animeJSON.url.animeflv
-    },
-    emision_semanal: {
-        dia_semana: animeJSON.emision_semanal.dia_semana,
-        hora: animeJSON.emision_semanal.hora,
-        minuto: animeJSON.emision_semanal.minuto
+const jobs: any[] = [];
+
+// Funcion que se ejecuta todos los dias a las 12pm para actualizar la lista de animes
+const actualizacion = schedule.scheduleJob({
+    hour: 12,
+    minute: 0
+}, function () {
+
+    if (jobs.length > 1) {
+
+        console.log(`Emisiones a borrar: ${jobs.length}.`)
+        jobs.forEach(job => {
+            job.cancel;
+            console.log(job + " cancelado");
+        })
+
+        jobs.length = 0;
+
+        console.log(`Emisiones restantes en arreglo: ${jobs.length}.`)
+
+    } else {
+        console.log(`No hay emisiones programadas, no se borrará ninguna.`)
     }
-}));
 
+    // Obtenemos todos los animes del archivo json y se guardan en arreglo
+    const animes: Anime[] = animesJSON.map((animeJSON: any) => ({
+        nombre: animeJSON.nombre,
+        descripcion: animeJSON.descripcion,
+        thumbnail: animeJSON.thumbnail,
+        url: {
+            animeflv: animeJSON.url.animeflv
+        },
+        emision_semanal: {
+            dia_semana: animeJSON.emision_semanal.dia_semana,
+            hora: animeJSON.emision_semanal.hora,
+            minuto: animeJSON.emision_semanal.minuto
+        }
+    }));
 
-animes.forEach(anime => {
-    schedule.scheduleJob({
-        hour: anime.emision_semanal.hora,
-        minute: anime.emision_semanal.minuto,
-        dayOfWeek: anime.emision_semanal.dia_semana
-    }, function () {
-        const botChannel = client.channels.cache.get(config.bot_channel_id)
+    // Creamos un arreglo vacio que contendra los animes del dia actual y tambien obtenemos el dia de la semana actual
+    const animesHoy: Anime[] = [];
+    const fechaActual = new Date();
+    const diaSemanaActual = fechaActual.getDay();
 
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('Animeflv')
-                    .setStyle('Link')
-                    .setURL(anime.url.animeflv),
-            );
+    animes.forEach(anime => {
 
-        const embed = new EmbedBuilder()
-            .setTitle(`[EMISION] ${anime.nombre}`)
-            .setDescription(anime.descripcion)
-            .setThumbnail(anime.thumbnail)
+        if (diaSemanaActual == anime.emision_semanal.dia_semana) {
+            animesHoy.push(anime);
+        }
 
-        botChannel.send({embeds: [embed], components: [row]});
     });
+
+    if (animesHoy.length > 0) {
+
+        animesHoy.forEach(anime => {
+
+            console.log(`${anime.nombre} agregado a la lista de emisiones.`)
+
+            // Funcion que se ejecuta a la hora determinada para anunciar el anime
+            let emision = schedule.scheduleJob({
+                hour: anime.emision_semanal.hora,
+                minute: anime.emision_semanal.minuto
+            }, function () {
+                const botChannel = client.channels.cache.get(config.bot_channel_id)
+
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel('Animeflv')
+                            .setStyle('Link')
+                            .setURL(anime.url.animeflv),
+                    );
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`[EMISION] ${anime.nombre}`)
+                    .setDescription(anime.descripcion)
+                    .setThumbnail(anime.thumbnail)
+
+                botChannel.send({embeds: [embed], components: [row]});
+                console.log(`${anime.nombre} emitido.`)
+            });
+
+            jobs.push(emision)
+
+        })
+
+    }
+
 });
+
+actualizacion.invoke();
 
 client.on('ready', () => {
     console.log("Bot online");
